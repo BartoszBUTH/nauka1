97523482998ef96be99569c69662997e9aeb2384
Bartosz <bartosz.borkowski@uth.pl>
2022-04-30 14:35:56 +0200

    hello

---
+++ script.js
@@ -0,0 +1,11 @@
+// const store = new Map([
+// 	['t-shirt', 20],
+// 	['jeans', 30],
+// 	['socks', 10],
+// 	['jacket', 60],
+// ]);
+
+// shopping = store.set('hat', 20);
+// store.forEach((value, key) => console.log(`${key}, ${value}`));
+
+console.log('hello');
